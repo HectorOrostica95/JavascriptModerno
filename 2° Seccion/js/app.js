a09cd10e8c6ba4732a7ee787e307944d2ad40cb1
HectorOrostica95 <hectororostica2.0@gmail.com>
2018-11-05 17:38:03 -0300

    Termino de la seccion 3

---
+++ 2° Seccion/js/app.js
@@ -0,0 +1,193 @@
+// Eliminar de Local Storage
+localStorage.clear();
+
+/*
+ 
+let elemento;
+
+elemento = document;
+elemento = document.all;
+elemento = document.all[10];
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+elemento = document.URL;
+elemento = document.characterSet;
+elemento = document.forms;
+elemento = document.forms[0];
+elemento = document.forms[0].id;
+elemento = document.forms[0].id;
+elemento = document.forms[0].className;
+elemento = document.forms[0].classList;
+// elemento = document.forms[0].classList[0];
+
+elemento = document.images;
+elemento = document.images[2];
+elemento = document.images[2].src;
+elemento = document.images[2].getAttribute("src");
+
+elemento = document.scripts;
+
+let imagenes = document.images;
+let imagenesArr = Array.from(imagenes);
+
+imagenesArr.forEach(function(imagen) {
+    console.log(imagen);
+});
+
+console.log(imagenesArr);
+
+let elemento;
+
+elemento = document.getElementById("hero");
+elemento = elemento.id;
+elemento = document.getElementById("hero").id;
+elemento = document.getElementById("hero").className;
+elemento = document.getElementById("header").className;
+
+console.log(elemento);
+let encabezado;
+
+encabezado = document.getElementById("encabezado");
+// encabezado = document.getElementById("encabezado").id;
+// encabezado = document.getElementById("encabezado").className;
+// encabezado = document.getElementById("encabezado").classList;
+// encabezado = document.getElementById("encabezado").textContent;
+// encabezado = document.getElementById("encabezado").innerText;
+
+encabezado.style.background = "#333";
+encabezado.style.color = "#fff";
+encabezado.style.padding = "20px";
+
+encabezado.textContent = "Los Mejores Cursos";
+encabezado.innerText = "Los Mejores Cursos";
+console.log(encabezado.innerText);
+
+//Query Selector
+
+const encabezado = document.querySelector("#encabezado");
+
+encabezado.style.background = "#333";
+encabezado.style.color = "#fff";
+encabezado.style.padding = "20px";
+encabezado.textContent = "Los Mejores Cursos";
+
+console.log(encabezado);
+
+//Query selector
+
+const encabezado = document.querySelector(".encabezado");
+const encabezado = document.querySelector("#encabezado");
+
+encabezado.style.background = "#333";
+encabezado.style.color = "#fff";
+encabezado.style.padding = "20px";
+encabezado.textContent = "Los Mejores Cursos";
+
+console.log(encabezado);
+
+const enlace =  document.querySelector(".enlace");
+const enlace =  document.querySelectorAll(".enlace");
+
+const enlace =  document.querySelector(".img");
+const enlace =  document.querySelector(".card img");
+
+console.log(enlace);
+
+let enlace; 
+
+enlace = document.querySelector("#principal a:first-child");
+enlace = document.querySelector("#principal a:nth-child(3)");
+// enlace = document.querySelector("#principal a:last-child");
+console.log(enlace);
+
+let enlaces;
+enlaces = document.getElementsByClassName('enlace');
+enlaces = document.getElementsByClassName('enlace')[3];
+
+enlaces.style.background = "#333";
+enlaces.textContent = "Nuevo enlace";
+console.log(enlaces);
+
+const listaEnlaces = document.querySelector("#principal");
+const listaEnlaces = document.querySelector("#principal").getElementsByClassName("enlace");
+console.log(listaEnlaces);
+
+const links = document.getElementsByTagName("a");
+
+links[18].style.color = "red";
+links[18].textContent = "Nuevo enlace";
+
+let enlaces = Array.from(links);
+
+enlaces.forEach(function(enlace){
+    console.log(enlace.textContent);
+})
+
+console.log(links);
+
+const enlaces = document.querySelectorAll ("#principal .enlace");
+
+enlaces[1].style.background = "red";
+enlaces[1].textContent = "Nuevo enlace";
+console.log (enlaces);
+//Con palabra reservada "odd" se regresa los impares
+const enlaces = document.querySelectorAll("#principal a:nth-child(odd)");
+
+enlaces.forEach(function (impares) {  
+    impares.style.backgroundColor = "red";
+    impares.style.color = "White";
+});
+console.log(enlaces);
+
+const navegacion = document.querySelector("#principal");
+
+// console.log(navegacion.childNodes);
+// console.log(navegacion.children);
+// console.log(navegacion.children[0]);
+// console.log(navegacion.nodeName);
+// console.log(navegacion.children[0].nodeName);
+// console.log(navegacion.children[0].nodeType);
+console.log(navegacion.children[0].textContent = "Nuevo enlace");
+
+// tipos de nodos 
+// 1   = Elementos
+// 2   = Atributos
+// 3   = Text Node
+// 8   = comentarios
+// 9   = documetos
+// 10  = doctype
+
+const barra = document.querySelector(".barra");
+console.log(barra.children);
+console.log(barra.children[0].children[0]);
+console.log(barra.children[0].children[0].children);
+
+const cursos = document.querySelectorAll(".card");
+console.log(cursos);
+console.log(cursos[0].lastElementChild);
+console.log(cursos[0].firstElementChild);
+console.log(cursos[0].childElementCount);
+
+const enlaces = document.querySelectorAll(".enlace");
+
+console.log(enlaces);
+console.log(enlaces[0].parentNode);
+console.log(enlaces[0].parentElement);
+console.log(enlaces[0].parentElement.parentElement);
+console.log(enlaces[0].parentElement.parentElement.parentElement);
+
+const cursos = document.querySelectorAll(".card");
+console.log(cursos[0]);
+console.log(cursos[0].parentElement);
+console.log(cursos[0].parentElement.parentElement);
+console.log(cursos[0].parentElement.parentElement.parentElement);
+console.log(cursos[0].parentElement.parentElement.parentElement.children[0].textContent = "Hola desde traversing");
+
+const enlaces = document.querySelectorAll(".enlace");
+
+console.log(enlaces[4].previousElementSibling.previousElementSibling);
+console.log(enlaces[0].nextElementSibling.nextElementSibling);
+*/
+
+//Esto continua en el archivo creando enlacesFooter
